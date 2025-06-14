7107c9eb517e16c0e5f8e1ce774b598699dd3e94
22tmason <22tmason@wakatipu.school.nz>
2025-06-14 19:28:45 +1200

    FULLY WORKING - PROGRESS!!!
    States are made in the game engine, and own a collection of provinces. Along with this, the province locator still works! The bug has finally been fixed!

--- scripts/state_importer.gd
+++ scripts/state_importer.gd
@@ -6,34 +6,34 @@ func _ready() -> void:
 func generate_states() -> void:
 	print("STARTING TO GENERATE STATES")
 
-	var state_folder := DirAccess.open("res://Global-Command-2.0/map/map_data/states/")
+	var state_folder = DirAccess.open("res://Global-Command-2.0/map/map_data/states/")
 	state_folder.list_dir_begin()
 	var file_name = state_folder.get_next()
 
 	while file_name != "":
-		if file_name.ends_with(".txt"):
-			var state_file := FileAccess.open("res://Global-Command-2.0/map/map_data/states/" + file_name, FileAccess.READ)
-			var file_content := state_file.get_as_text().strip_edges()
-			state_file.close()
+		var state_file = FileAccess.open("res://Global-Command-2.0/map/map_data/states/" + file_name, FileAccess.READ)
+		var file_content = state_file.get_as_text().strip_edges()
+		state_file.close()
 
-			# Extract state ID
-			var from = file_content.find("id=") + "id=".length()
-			var to = file_content.find("\n", from)
-			var state_id = int(file_content.substr(from, to - from).strip_edges())
+		# ID
+		var from = file_content.find("id=") + 3
+		var to = file_content.find("name=") - from
+		var id = int(file_content.substr(from, to))
 
-			# Extract state name
-			from = file_content.find("name=") + "name=".length()
-			to = file_content.find("\n", from)
-			var state_name = str(file_content.substr(from, to - from).replace("\"", "").strip_edges())
+		# Name
+		from = file_content.find("name=") + 5
+		to = file_content.find("provinces=") - from
+		var state_name = str(file_content.substr(from, to)).replace("\"", "")
 
-			# Extract provinces
-			from = file_content.find("provinces=") + "provinces=".length()
-			to = file_content.find("}", from)
-			var provinces_raw = file_content.substr(from, to - from).strip_edges()
-			var provinces = provinces_raw.split(" ")
+		# Provinces
+		from = file_content.find("provinces={") + 11
+		to = file_content.find("}") - from
+		var provinces = file_content.substr(from, to).strip_edges().split(" ")
 
-			print("STATE_%d" % state_id)
-			print(state_name)
-			print(provinces)
+		print(id)
+		print(state_name)
+		print(provinces)
 
 		file_name = state_folder.get_next()
+
+	state_folder.list_dir_end()
